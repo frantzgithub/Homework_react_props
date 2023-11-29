23145d2374da73884c485a0c2bd34cc84401ea9f
lin <139031054+frantzgithub@users.noreply.github.com>
2023-11-29 19:11:10 -0400

    homework props react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {StudentCard} from './components/StudentCard';
+
+function App() {
+
+  return (
+    <>
+      <StudentCard name="Berlin" age={19} country="Haiti" />
+      <StudentCard name="Frantz" age={20} country="Saint-Domingue" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StudentCard/index.jsx
@@ -0,0 +1,10 @@
+
+export const StudentCard = ({name, age, country}) => {
+    return (
+        <div>
+          <h2>Name: {name}</h2>
+          <p>Age: {age}</p>
+          <p>Country: {country}</p>
+        </div>
+        );
+}
